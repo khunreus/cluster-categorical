7a3c769ac6e874d66f3cfca632a088b3e546d33d
khunreus <anastasia.reusova@gmail.com>
2018-03-22 18:28:30 +0700

    summarize clustering stats

---
+++ clustering.stats.R
@@ -0,0 +1,56 @@
+# Cluster stats comes in a list form, it is more convenient to look at it as a table
+# This code below will produce a dataframe with observations in columns and variables in row
+# Not quite tidy data, but it's nicer to look at
+
+cstats.table <- function(dist, tree, k) {
+clust.assess <- c("cluster.number","n","average.within","average.between",
+                    "wb.ratio","dunn2","avg.silwidth")
+clust.size <- c("cluster.size")
+stats.names <- c()
+row.clust <- c()
+
+output.stats <- matrix(ncol = k, nrow = length(clust.assess))
+cluster.sizes <- matrix(ncol = k, nrow = k)
+
+for(i in c(1:k)){
+  row.clust[i] <- paste("Cluster-", i, " size")
+}
+
+for(i in c(2:k)){
+  stats.names[i] <- paste("Test", i-1)
+  
+  for(j in seq_along(clust.assess)){
+    output.stats[j, i] <- unlist(cluster.stats(d = dist, clustering = cutree(tree, k = i))[clust.assess])[j]
+    
+  }
+  
+  for(d in 1:k) {
+    cluster.sizes[d, i] <- unlist(cluster.stats(gower.dist, cutree(divisive.clust, i))[clust.size])[d]
+    dim(cluster.sizes[d, i]) <- c(length(cluster.sizes[i]), 1)
+    cluster.sizes[d, i]
+    
+  }
+}
+
+output.stats.df <- data.frame(output.stats)
+
+cluster.sizes <- data.frame(cluster.sizes)
+cluster.sizes[is.na(cluster.sizes)] <- 0
+
+rows.all <- c(clust.assess, row.clust)
+# rownames(output.stats.df) <- clust.assess
+output <- rbind(output.stats.df, cluster.sizes)[ ,-1]
+colnames(output) <- stats.names[2:k]
+rownames(output) <- rows.all
+
+is.num <- sapply(output, is.numeric)
+output[is.num] <- lapply(output[is.num], round, 2)
+
+output
+}
+
+# I am capping the maximum amout of clusters by 5
+# I want to choose a reasonable number, based on which I will be able to see basic differences between customer groups
+
+cstats.table(gower.dist, divisive.clust.c, 5)
+cstats.table(gower.dist, aggl.clust.c, 5) #complete linkages looks like the most balanced approach

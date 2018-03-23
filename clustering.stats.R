37e1349dfb326f4e8411adcaac8dbbf2db0f2f46
khunreus <anastasia.reusova@gmail.com>
2018-03-23 14:12:07 +0700

    to add within SS / amend # observations in clusters

--- clustering.stats.R
+++ clustering.stats.R
@@ -1,10 +1,11 @@
 # Cluster stats comes in a list form, it is more convenient to look at it as a table
 # This code below will produce a dataframe with observations in columns and variables in row
 # Not quite tidy data, but it's nicer to look at
+library(fpc)
 
 cstats.table <- function(dist, tree, k) {
-clust.assess <- c("cluster.number","n","average.within","average.between",
-                    "wb.ratio","dunn2","avg.silwidth")
+clust.assess <- c("cluster.number","n","within.cluster.ss","average.within","average.between",
+                  "wb.ratio","dunn2","avg.silwidth")
 clust.size <- c("cluster.size")
 stats.names <- c()
 row.clust <- c()
@@ -25,7 +26,7 @@ for(i in c(2:k)){
   }
   
   for(d in 1:k) {
-    cluster.sizes[d, i] <- unlist(cluster.stats(gower.dist, cutree(divisive.clust, i))[clust.size])[d]
+    cluster.sizes[d, i] <- unlist(cluster.stats(d = dist, clustering = cutree(tree, k = i))[clust.size])[d]
     dim(cluster.sizes[d, i]) <- c(length(cluster.sizes[i]), 1)
     cluster.sizes[d, i]
     
@@ -52,5 +53,7 @@ output
 # I am capping the maximum amout of clusters by 5
 # I want to choose a reasonable number, based on which I will be able to see basic differences between customer groups
 
-cstats.table(gower.dist, divisive.clust.c, 5)
-cstats.table(gower.dist, aggl.clust.c, 5) #complete linkages looks like the most balanced approach
+stats.df.divisive <- cstats.table(gower.dist, divisive.clust, 7)
+stats.df.aggl <-cstats.table(gower.dist, aggl.clust.c, 7) #complete linkages looks like the most balanced approach
+stats.df.divisive
+stats.df.aggl
